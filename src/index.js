d3e381a5c4cbaf48651a26311b9d1581ac0db988
Joaquin Obregon <joaquinobregon@MacBook-Pro-de-Joaquin.local>
2026-06-09 21:39:38 -0400

    fix: reconnect vercel

--- src/index.js
+++ src/index.js
@@ -5,3 +5,4 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode><App /></React.StrictMode>);
 // Tue Jun  9 21:07:59 -04 2026
 // cache bust Tue Jun  9 21:24:27 -04 2026
+// reconnect Tue Jun  9 21:39:38 -04 2026
