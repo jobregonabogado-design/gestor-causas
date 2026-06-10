bd0aacf60592da8c9b72c337f37f9431fc0f3e51
Joaquin Obregon <joaquinobregon@MacBook-Pro-de-Joaquin.local>
2026-06-09 21:24:27 -0400

    fix: clean rebuild

--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode><App /></React.StrictMode>);
 // Tue Jun  9 21:07:59 -04 2026
+// cache bust Tue Jun  9 21:24:27 -04 2026
